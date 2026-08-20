8739abbb6f57cef9200ac9f5e5f37c85afab59cf
rodrigof981 <rodrigof981@gmail.com>
2026-08-20 01:05:39 -0300

    fix: replace PrismaClient instantiation with imported Prisma instance in middleware and repository

--- backend/dist/repositories/activity.repository.js
+++ backend/dist/repositories/activity.repository.js
@@ -1,14 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ActivityRepository = void 0;
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
+const prisma_1 = __importDefault(require("../lib/prisma"));
 class ActivityRepository {
     async create(data) {
-        return prisma.activityLog.create({ data });
+        return prisma_1.default.activityLog.create({ data });
     }
     async findAllByUser(userId) {
-        return prisma.activityLog.findMany({
+        return prisma_1.default.activityLog.findMany({
             where: { userId },
             orderBy: { createdAt: 'desc' },
             take: 10,
